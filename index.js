183a1057de8103b54ef82f9f2f85832d30cd08f2
Brian Tanner <brian.tanner@campingworld.com>
2016-03-24 02:37:33 -0400

    added members query, config file loading, write to data to chats.json

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const util = require('util');
 const path = require('path');
 const async = require('async');
+const jsonfile = require('jsonfile');
 const readline = require('readline');
 const sqlite3 = require('sqlite3').verbose();
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
@@ -13,10 +14,24 @@ const constants = {
   IS_WIN: /^win/.test(process.platform),
   IS_MAC: /darwin/.test(process.platform),
   IS_LINUX: (!this.IS_WIN || !this.IS_MAC),
-  DIR_SEP: this.IS_WIN ? "\\" : "/"
+  DIR_SEP: this.IS_WIN ? "\\" : "/",
+  BOT_CFG: path.join('..', 'bot.cfg'),
+  CHAT_CFG: path.join('./', 'chats.json')
 };
 
+if (!fs.existsSync(constants.BOT_CFG)) {
+  console.error("Can't find bot.cfg.");
+}
+
+if (!fs.existsSync(constants.CHAT_CFG)) {
+  fs.closeSync(fs.openSync(constants.CHAT_CFG, 'w'));
+  fs.writeFileSync(constants.CHAT_CFG, "{}", 'utf8');
+  console.log("Created chats.json");
+}
+
 let mainDb, db, username,
+    botConfig = jsonfile.readFileSync(constants.BOT_CFG),
+    chatConfig = jsonfile.readFileSync(constants.CHAT_CFG),
     mainPath = (constants.IS_WIN) ? path.join(constants.HOME_DIR, "AppData", "Roaming", "Skype") : 
                (constants.IS_MAC) ? path.join(constants.HOME_DIR, "Library", "Application Support", "Skype") :
                /**  IS LINUX  **/   path.join(constants.HOME_DIR, ".Skype");
@@ -39,6 +54,7 @@ async.waterfall([
       }
 
       username = _username;
+      // console.log(botConfig);
       return callback(null);
     });
   },
@@ -48,7 +64,7 @@ async.waterfall([
     mainDb = path.join(mainPath, username, "main.db");
     console.log(mainDb);
 
-    let query = "SELECT identity, meta_topic FROM Conversations WHERE identity LIKE '19:%@p2p.thread.skype'";
+    let query = "SELECT id, identity, meta_topic FROM Conversations WHERE identity LIKE '19:%@p2p.thread.skype'";
 
     // create db instance
     db = new sqlite3.Database(mainDb);
@@ -57,18 +73,50 @@ async.waterfall([
       // list cloud p2p chats
       db.all(query, (err, rows) => {
         if (err) { return callback(err); }
+        let chats = [];
 
         rows.forEach(row => {
           let ident = row.identity.split(':')[1].split('@')[0];
           row.ident_decoded = base64.decode(ident);
 
-          console.log(row);
+          if (botConfig.monitor[row.ident_decoded]) {
+            row.monitor = botConfig.monitor[row.ident_decoded];
+            chats.push(row);
+          }
+
         });
         
-        return callback(null);
+        return callback(null, chats);
 
       });
 
+    });
+  },
+
+  function (chats, callback) {
+    let query;
+
+    async.eachSeries(chats, (chat, callback) => {
+      query = util.format("SELECT identity, role FROM ChatMembers where chatname = '%s'", chat.ident_decoded);
+
+      db.serialize(() => {
+        // get chat members
+        db.all(query, (err, rows) => {
+          chat.members = rows;
+          chat.admins = rows.filter(o => { return o.role === 2; });
+          return callback();
+        });
+      });
+    }, () => {
+      return callback(null, chats);
+    });
+  },
+
+  function (chats, callback) {
+    console.log(util.format("Writing %d chats to file.", chats.length));
+    jsonfile.writeFile(constants.CHAT_CFG, chats, {spaces: 2}, (err) => {
+      if (err) { return callback(err); }
+      return callback(null);
     });
   }
 
